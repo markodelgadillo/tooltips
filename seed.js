8c63e6f1f68bd83bb27efdda8492899c79b6e662
marko delgadillo <markoneseven@gmail.com>
2017-10-26 13:27:19 -0700

    #1 Completed the seed file with data.

---
+++ seed.js
@@ -0,0 +1,22 @@
+const { MongoClient } = require('mongodb')
+
+const seed = (module.exports = MongoClient.connect(
+  'mongodb://localhost/library',
+  function(err, db) {
+    if (err) {
+      console.error(err)
+      process.exit(1)
+    }
+    const tooltip = db.collection('tooltip')
+
+    tooltip.deleteMany()
+
+    tooltip.insertMany([
+      {
+        id: 'tooltip',
+        content: 'This is a tooltip.'
+      }
+    ])
+    db.close()
+  }
+))

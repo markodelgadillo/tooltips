58159cedad2f16e8f51fb7c6325d45fe6f55fd14
marko delgadillo <markoneseven@gmail.com>
2017-10-26 13:30:25 -0700

    #1 Commiting the server file that successfully connects to the database.

---
+++ index.js
@@ -0,0 +1,26 @@
+const { MongoClient } = require('mongodb')
+const express = require('express')
+const seed = require('./seed.js')
+const app = express()
+const path = require('path')
+app.use(express.static('./public'))
+
+MongoClient.connect('mongodb://localhost/library', function(err, db) {
+  if (err) {
+    console.error(err)
+    process.exit(1)
+  }
+  const tooltip = db.collection('tooltip')
+
+  app.get('/tooltip', function(req, res) {
+    console.log('Getting stuff...')
+    tooltip
+      .find({}, { _id: 0 })
+      .toArray()
+      .then(response => res.json(response[0].content))
+  })
+
+  app.listen(9999, function() {
+    console.log('Listening on port 9999.')
+  })
+})

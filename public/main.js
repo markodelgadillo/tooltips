b327340fcf7d9e2ef4cd0042cf0136e3c81c05ba
marko delgadillo <markoneseven@gmail.com>
2017-10-26 16:02:48 -0700

    #1 Each button now displays the tooltip message from the database upon hover.

--- public/main.js
+++ public/main.js
@@ -1,18 +1,33 @@
-function renderTip(tip) {
-  $button.setAttribute('data-tip', tip)
+function renderTip(i, tip) {
+  $button[i].setAttribute('data-tip', tip)
 }
 
-const $button = document.querySelector('.button')
+const $button = document.querySelectorAll('.button')
 
-$button.addEventListener('mouseenter', function(event) {
-  const { top, bottom, left, right } = event.target.getBoundingClientRect()
-  console.log(event.target)
-  const target = () => event.target
-  const tooltip = () => fetch('/tooltip').then(res => res.json())
-  tooltip().then(tip => renderTip(tip))
+for (let i = 0; i < $button.length; i++) {
+  $button[i].addEventListener('mouseenter', function(event) {
+    console.log(event.target)
 
-  console.log(`${top} px from the top of the window`)
-  console.log(`${window.innerHeight - bottom}px from the bottom of the window`)
-  console.log(`${left}px from the left of the window`)
-  console.log(`${window.innerWidth - right} from the right of the window`)
-})
+    const tooltip = () => fetch('/tooltip').then(res => res.json())
+
+    tooltip().then(tip => renderTip(i, tip))
+  })
+}
+
+/*
+const { top, bottom, left, right } = event.target.getBoundingClientRect()
+console.log(`${top} px from the top of the window`)
+console.log(`${window.innerHeight - bottom}px from the bottom of the window`)
+console.log(`${left}px from the left of the window`)
+console.log(`${window.innerWidth - right} from the right of the window`)
+*/
+/*
+var $buttons = document.querySelectorAll('.button')
+for (var i = 0; i < $buttons.length; i++) {
+  $buttons[i].addEventListener('mouseenter', function(event) {
+    const tooltip = () => fetch('/tooltip').then(res => res.json())
+
+    tooltip().then(tip => renderTip(tip))
+  })
+}
+*/
